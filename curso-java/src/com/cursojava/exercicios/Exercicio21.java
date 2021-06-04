e0187acdd50559a9a5c7db7eb44192e32d4f989b
13jaque <jaquelinegidelfonso@gmail.com>
2021-06-03 22:16:15 -0300

    atualizando 1

--- curso-java/src/com/cursojava/exercicios/Exercicio21.java
+++ curso-java/src/com/cursojava/exercicios/Exercicio21.java
@@ -1,4 +1,39 @@
 package com.cursojava.exercicios;
 
+import java.util.Scanner;
+
 public class Exercicio21 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Entre com o numero de turmas: ");
+        int turmas = scan.nextInt();
+
+        int alunos;
+        int soma = 0;
+        boolean invalido = false;
+
+        for (int i = 1; i <=turmas; i++){
+
+            invalido = false;
+            do {
+                System.out.println("Entre com o numero de alunos p/ turmas: " +i);
+                alunos = scan.nextInt();
+
+                if (alunos <= 40){
+                    invalido = true;
+                }else {
+                    System.out.println("Numeros de alunos invalido.");
+                }
+            }while (!invalido);
+
+            soma += alunos;
+        }
+
+        double media = soma / turmas;
+
+        System.out.println("Media: " + media);
+    }
 }
---
+++ curso-java/src/com/cursojava/exercicios/aula16e17/Exercicio22.java
@@ -0,0 +1,4 @@
+package com.cursojava.exercicios.aula16e17;
+
+public class Exercicio22 {
+}
